f59ad8676510ee74fbdb32b06c846d0e114d22ae
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-09-30 00:22:32 -0300

    Tópicos

---
+++ Tópico01/script.js
@@ -0,0 +1,14 @@
+// Declarar uma variável e atribuir o seu nome
+var nome = 'Zé';
+
+//Declarar uma variável e atribuir sua idade
+let idade = 75;
+
+//Declarar uma variável e atribuir sua comida favorita
+const comidaFavorita = 'Arroz, feijão, frango e salada';
+
+//Declarar 3 variáveis sem valores
+var uma;
+let dois, tres;
+
+console.log(nome,idade,comidaFavorita);
---
+++ Tópico02/script.js
@@ -0,0 +1,13 @@
+//Declare uma variável contendo uma string 
+
+//Declare uma variável contendo um número dentro de uma string
+
+//Declare uma variável com a sua idade
+
+//Declare outra variável para contar a frase: Meu nome é SEU NOME e minha idade é SUA IDADE
+
+//Declare outras duas variáveis, uma com seu sobrenome e com seu nome exiba as mesmas juntas
+
+//Coloque a seguinte frase em uma variável: It's study tima
+
+//Verifique o tipo da variável que contém o seu nome
